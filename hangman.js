8cdef3455323ca47395572685af6fe9a33b2b169
grady407 <grady407@gmail.com>
2018-01-20 07:53:36 -0500

    update

---
+++ hangman.js
@@ -0,0 +1,4 @@
+
+
+// *******
+// I honestly didnt know where to start with this assignment and I didnt have the time or energy to attempt to code something as my personal life was alot different this week.
